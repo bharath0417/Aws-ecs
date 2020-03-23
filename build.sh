37331b6b8a587578d0480e13bde6a5b254231cdd
Chirlabharath Kumarreddy <chirlabharath.kumarreddy@publicissapient.com>
2020-03-23 16:30:51 +0530

    change the aws info

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t aws-demo:v_$BUILD_NUMBER --pull=true /var/lib/jenkins/workspace/aws-ecs-app \
-&& docker tag  localhost/aws-demo:v_$BUILD_NUMBER 405545836457.dkr.ecr.us-east-1.amazonaws.com/ecs-demo:v_$BUILD_NUMBER \
-&& docker push 405545836457.dkr.ecr.us-east-1.amazonaws.com/ecs-demo:v_$BUILD_NUMBER
+&& docker tag  localhost/aws-demo:v_$BUILD_NUMBER docker uri:v_$BUILD_NUMBER \
+&& docker push dockeruri:v_$BUILD_NUMBER
 
 
 
